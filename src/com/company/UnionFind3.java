7717b0291854e52e80b1723acdb96fc644239f6f
zzzjh <718375890@qq.com>
2020-03-16 21:01:24 +0800

    Initial commit

---
+++ src/com/company/UnionFind3.java
@@ -0,0 +1,51 @@
+//Quick Union 基于树的节点数的优化
+public class UnionFind3 implements UF{
+
+    private int[] parent;
+    private int[] sz;
+
+    public UnionFind3(int size){
+
+        parent = new int[size];
+        sz = new int[size];
+        for(int i=0; i<parent.length; i++){
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize(){return parent.length;}
+
+    private int find(int p){
+
+        if(p<0 && p>=parent.length)
+            throw new IllegalArgumentException(p + " is out of bound.");
+
+        while(p != parent[p]){
+            p = parent[p];
+        }
+        return p;
+    }
+
+    @Override
+    public boolean isConnected(int p, int q){return find(p) == find(q);}
+
+    @Override
+    public void unionElements(int p, int q){
+
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if(pRoot == qRoot)
+            return;
+        if(sz[pRoot] < sz[qRoot]){
+            parent[pRoot] = qRoot;
+            sz[qRoot] += pRoot;
+        }
+        else{
+            parent[qRoot] = pRoot;
+            sz[pRoot] += qRoot;
+        }
+    }
+}
